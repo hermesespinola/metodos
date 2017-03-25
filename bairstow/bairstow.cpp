67db4c3677a3c6a8b9b825ed66025fe2490afde2
Hermes Espínola <hermesespinola@gmail.com>
2017-03-25 13:23:39 -0600

    Final bairstow

--- bairstow/bairstow.cpp
+++ bairstow/bairstow.cpp
@@ -1,67 +1,180 @@
-#include <cstdio>
-#include <cmath>
 #include <iostream>
+#include <vector>
+#include <cmath>
 
-typedef double (* vFunctionCall)(double x);
+using namespace std;
 
-// a - array of coeficients
-// d - degree of the polynomial function
-// r -
-// s -
-double bairstow(vFunctionCall fun, double *a, int d, double r, double s) {
-  double dn, dr, ds, drn, dsn, eps;
-  int iter;
-  double *b = (double *) malloc(d * sizeof(double));
-  double *c = (double *) malloc(d * sizeof(double));
-  r = s = 0;
-  dr = 1;
-  ds = 0;
-  eps = 1e-14;
-  iter = 1;
+struct complex_number {
+  double r, i;
+};
 
-  while ((abs(dr) + fabs(ds) > eps)) {
-    b[1] = a[1] - r;
-    c[1] = b[1] - r;
+void synthetic_division(vector<double> &pol, vector<double> &res, vector<double> &aux, double r, double s) {
+  int i, j, degree = pol.size() - 1;
 
-    for (int i = 2; i <= d; i++) {
-      b[i] = a[i] - r * b[i - 1] - s * b[i - 2];
-      c[i] = b[i] - r * c[i - 1] - s * c[i - 2];
+  for (i = degree, j = 0; i >= 0; i--, j++) {
+    if (i == degree) {
+      res[j] = pol[j];
+    } else if (i == degree - 1) {
+      res[j] = pol[j] + res[j - 1] * r;
+    } else {
+      res[j] = pol[j] + res[j - 1] * r + res[j - 2] * s;
     }
-
-    dn=c[d-1] * c[d-3] - c[d-2] * c[d-2];
-    drn = b[d] * c[d-3] - b[d-1] * c[d-2];
-		dsn = b[d-1] * c[d-1] - b[d] * c[d-2];
-
-    if (abs(dn) < 1e-16) {
-      dn = drn = dsn = 1;
-    }
-
-    dr = drn / dn;
-    ds = dsn / dn;
-
-    r += dr;
-    s += ds;
-    iter++;
-  }
-  for (int i = 0; i < d - 1; i++) {
-    a[i] = b[i];
   }
 
-  a[d] = s;
-  a[d - 1] = r;
-  return 0;
+  for (i = degree, j = 0; i >= 0; i--, j++) {
+    if (i == degree) {
+      aux[j] = res[j];
+    } else if (i == degree - 1) {
+      aux[j] = res[j] + aux[j - 1] * r;
+    } else {
+      aux[j] = res[j] + aux[j - 1] * r + aux[j - 2] * s;
+    }
+  }
 }
 
-double pol(double x, double *c, int degree) {
-  double result;
-  for (int i = 0; i < degree; i++) {
-    result += c[i] * pow(x, degree - i);
+void bairstow(vector<double> &pol, vector<complex_number*> &roots, double pi, double qi, double err) {
+  while (pol.size() > 3) {
+    vector<double> res(pol.size(), 0.0);
+    vector<double> aux(pol.size(), 0.0);
+
+    double p = pi;
+    double q = qi;
+    double dp = 0.0;
+    double dq = 0.0;
+    double error_p = 1.0;
+    double error_q = 1.0;
+
+    do {
+      // Initialize aux arrays to 0
+      for (unsigned i = 0; i < res.size(); i++) {
+        res[i] = 0;
+        aux[i] = 0;
+      }
+
+      synthetic_division(pol, res, aux, p, q);
+
+      cout << "Result (B) array: {";
+      for (unsigned i = 0; i < res.size(); i++) {
+        cout << res[i];
+        if (i < res.size()-1){
+          cout << ", ";
+        }
+      }
+      cout << "}" << endl << endl;
+
+      int deg = pol.size() - 1;
+      double d = aux[deg - 2] * aux[deg - 2] - aux[deg - 1] * aux[deg - 3];
+      dp = (-res[deg - 1] * aux[deg - 2] + res[deg] * aux[deg - 3]) / d;
+      dq = (-aux[deg - 2] * res[deg] + aux[deg - 1] * res[deg - 1]) / d;
+
+      error_p = dp / p;
+      error_q = dq / q;
+      p += dp;
+      q += dq;
+    } while (abs(error_p) > err && abs(error_q) > err);
+
+    cout << endl;
+    // 2 Roots
+    double det = pow(p, 2) + 4 * q;
+
+    complex_number *r1 = new complex_number;
+    complex_number *r2 = new complex_number;
+    if (det < 0) {
+      r1->r = p / 2;
+      r1->i = sqrt(-det) / 2;
+      r2->r = p / 2;
+      r2->i = -sqrt(-det) / 2;
+    }
+    else {
+      r1->r = (p + sqrt(det)) / 2;
+      r1->i = 0;
+      r2->r = (p - sqrt(det)) / 2;
+      r2->i = 0;
+    }
+    roots.push_back(r1);
+    roots.push_back(r2);
+
+    res = vector<double>(pol.size(), 0.0);
+    aux = vector<double>(pol.size(), 0.0);
+
+    synthetic_division(pol, res, aux, p, q);
+
+    // Adjust polynomial
+    for (unsigned i = 0; i < pol.size() - 2; i++) {
+      pol[i] = res[i];
+    }
+    pol.pop_back();
+    pol.pop_back();
+
+
+    cout << "Polynomial Update" << endl << "------------------------------" << endl;
+    cout << "{";
+    for (unsigned i = 0; i < pol.size(); i++) {
+      cout << pol[i];
+      if (i < pol.size()-1){
+        cout << ", ";
+      }
+    }
+    cout << "}" << endl;
   }
-  return result + c[degree - 1];
+
+
+  if (pol.size() == 3) {
+    int p = -pol[1];
+    int q = -pol[2];
+    double det = pow(p, 2) + 4 * q;
+
+    complex_number *r1 = new complex_number;
+    complex_number *r2 = new complex_number;
+    if (det < 0) {
+      r1->r = p / 2;
+      r1->i = sqrt(-det) / 2;
+      r2->r = p / 2;
+      r2->i = -sqrt(-det) / 2;
+    }
+    else {
+      r1->r = (p + sqrt(det)) / 2;
+      r1->i = 0;
+      r2->r = (p - sqrt(det)) / 2;
+      r2->i = 0;
+    }
+    roots.push_back(r1);
+    roots.push_back(r2);
+  }
+  else if (pol.size() == 2) {
+    complex_number *r = new complex_number;
+    r->r = -pol[1] / pol[0];
+    r->i = 0;
+    roots.push_back(r);
+  }
+}
+
+void print_pol(double *coeficients, unsigned degree) {
+  for (unsigned i = 0; i < degree - 1; i++) {
+    cout << coeficients[i] << "x^" << degree - i - 1 << " + ";
+  }
+  cout << coeficients[degree - 1] << '\n';
 }
 
 int main(int argc, char const *argv[]) {
-  int length = 5;
-  double *c = new double[length] {1,2,3,4,5};
-  std::cout << pol(2, c, 5) << '\n';
+  double coeficients[] = {6.7, -4, 0, 3.2, -3, 8, 1.2, 2, 0, 1, -4};
+  vector<double> polynomial(coeficients, coeficients + sizeof(coeficients) / sizeof(double));
+  vector<complex_number *> roots;
+  double p = -1, q = -1;
+
+  cout << "Original polynomial: ";
+  print_pol(coeficients, polynomial.size());
+
+  cout << '\n' << "Bairstow: " << '\n';
+  bairstow(polynomial, roots, p, q, 1e-4);
+
+  std::cout << "Roots:" << '\n';
+  for (unsigned i = 0; i < roots.size(); i++) {
+    if (roots[i]->i != 0) {
+      cout << roots[i]->r << " + " << roots[i]->i << "i" << endl;
+    }
+    else {
+        cout << roots[i]->r << endl;
+    }
+  }
 }
