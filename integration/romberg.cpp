9b962585454326c715389f52fa7a961f375c2113
Hermes Espínola <hermes.espinola@gmail.com>
2017-05-05 15:38:58 -0500

    romberg interpolation method

--- integration/romberg.cpp
+++ integration/romberg.cpp
@@ -1,11 +1,64 @@
 #include <iostream>
+#include <cmath>
 
 using namespace std;
 
-double romberg() {
-    
+double f(double x){
+  return exp( x );
 }
 
-int main () {
-    return 0;
+double trapezoid(double a, double b, int n) {
+  double h = (b - a) / n;
+  double sum = 0;
+  for(double i = a + h; i < b; i += h) {
+    sum += 2*f(i);
+  }
+  sum += f(b) + f(a);
+  sum *= h/2;
+  cout << "Trapezoid Approximation = " << sum << endl;
+  return sum;
+}
+
+double romberg_help(double a, double b, int k) {
+  return ( pow(4,k)*b - a ) / ( pow(4,k) - 1 );
+}
+
+void print_array(double *a, int n) {
+  cout << "[";
+  for(int i = 0; i < n - 1; i++) {
+    cout << a[i] << ",";
+  }
+  cout << a[n - 1] << "]" << endl;
+}
+
+double romberg(int no_of_approximations, double a, double b) {
+  int *number = new int[no_of_approximations];
+  double *approximations = new double[no_of_approximations];
+  for(int i = 0; i < no_of_approximations; i++) {
+    cout << "Number of Trapezoids for approximation "<< i <<" = ";
+    cin >> number[i];
+  }
+
+  for(int i = 0; i < no_of_approximations; i++) {
+    approximations[i] = trapezoid(a, b, number[i]);
+  }
+
+  for(int j = 1; j < no_of_approximations; j++) {
+    for(int i = 0; i < no_of_approximations - j; i++) {
+      approximations[i] = romberg_help(approximations[i], approximations[i+1], j);
+      //print_array(approximations, no_of_approximations);
+    }
+  }
+  cout << "Integral = " << approximations[0] << endl;
+  return approximations[0];
+}
+
+int main() {
+  double a, b;
+  int no_of_approximations;
+  cout << "Initial value (a): "; cin >> a;
+  cout << "Final value (b): "; cin >> b;
+  cout << "Number of Initial Approximations: "; cin >> no_of_approximations;
+  romberg(no_of_approximations, a, b);
+  return 0;
 }
